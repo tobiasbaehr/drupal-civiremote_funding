74b2a2a006da68c8da6544311a9bd72a6b0cce0d
Dominic Tubach <tubach@systopia.de>
2023-08-25 16:41:40 +0200

    Add summary applications to dashboard

--- src/Plugin/Block/CiviremoteFundingDashboardGroupApplications.php
+++ src/Plugin/Block/CiviremoteFundingDashboardGroupApplications.php
@@ -50,6 +50,14 @@ final class CiviremoteFundingDashboardGroupApplications extends BlockBase {
         ],
         [
           '#type' => 'civiremote_funding_dashboard_element',
+          '#title' => $this->t('My Summary Applications'),
+          '#url' => Url::fromUri('base:civiremote/funding/case'),
+          '#content' => [
+            '#markup' => '<div>' . $this->t('Manage current summary applications') . '</div>',
+          ],
+        ],
+        [
+          '#type' => 'civiremote_funding_dashboard_element',
           '#title' => $this->t('New Application'),
           '#url' => Url::fromUri('base:civiremote/funding/application/add'),
           '#content' => [
